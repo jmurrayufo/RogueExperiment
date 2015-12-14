fd24c32420ce98a0ec59da315c0ff91a2eef6590
John Murray <jmurrayufo@gmail.com>
2015-12-14 11:22:08 -0700

    Creation of basic logging

--- RogueE.py
+++ RogueE.py
@@ -1,5 +1,56 @@
 #!/usr/bin/env python
 
 import curses
+import os
+import logging
+import datetime
 
-print "Hello User!"
+class App():
+   def __init__( s ):
+      s.Running = True
+      s.Logging = logging.getLogger( '' )
+
+   def Run( s ):
+      while s.Running:
+         s.Main()
+      s.Logging.info("Terminate Application")
+
+   def Main( s ):
+      s.Running = False
+
+
+      
+
+if __name__ == '__main__':
+   
+   scriptPath = os.path.dirname( os.path.realpath(__file__) ) + "/"
+
+   # create logger with 'spam_application'
+   logger = logging.getLogger('')
+   logger.setLevel(logging.DEBUG)
+   
+   # Make sure we have a logging directory
+   if not os.path.isdir( scriptPath + 'logs' ):
+      os.mkdir( scriptPath + 'logs' )
+
+   # create file handler which logs even debug messages
+   fh = logging.FileHandler(scriptPath + 'logs/RogueE-{}.log' .format( datetime.date.today() ) )
+   fh.setLevel(logging.DEBUG)
+   
+   # create console handler with a higher log level
+   ch = logging.StreamHandler()
+   ch.setLevel(logging.ERROR)
+   
+   # create formatter and add it to the handlers
+   formatter = logging.Formatter('%(asctime)s - %(module)s:%(lineno)d:%(name)s - %(levelname)s - %(message)s')
+   fh.setFormatter(formatter)
+   #ch.setFormatter(formatter)
+   
+   # add the handlers to the logger
+   logger.addHandler(fh)
+   logger.addHandler(ch)
+
+   logger.info('test')
+
+   x = App()
+   x.Run()
